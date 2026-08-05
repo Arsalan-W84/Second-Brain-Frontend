61a45df216fa54caebc894314b03115e15f68574
Arsalan-W84 <arsalanwahid0804@gmail.com>
2026-08-05 20:26:41 +0000

    Fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react() , tailwindcss()],
-  base : process.env.VITE_BASE_PATH || "/", 
+  base : process.env.VITE_BASE_PATH || "/Second-Brain-Frontend", 
   server : {
     allowedHosts : [
       "malacotic-jetta-melodramatically.ngrok-free.dev"
